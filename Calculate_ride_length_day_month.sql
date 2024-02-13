8a1c93a2b087e6fa8d2e66a9041f3c61e7ab4a94
DataRosh <159941547+DataRosh@users.noreply.github.com>
2024-02-13 14:06:10 -0500

    Delete Calculate_ride_length_day_month.sql

--- Calculate_ride_length_day_month.sql
+++
@@ -1,2 +0,0 @@
-SELECT *, ended_at - started_at AS ride_length, EXTRACT(DAYOFWEEK FROM started_at) AS day_of_the_week, EXTRACT (MONTH FROM started_at) AS month
-FROM elite-bedrock-410313.Bicycle_Case_2023_full.Combined_2023_Data
