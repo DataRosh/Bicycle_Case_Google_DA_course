a43cd88b031961aa636d57a4b5ce53031a694472
DataRosh <159941547+DataRosh@users.noreply.github.com>
2024-02-20 16:21:39 -0500

    Delete Attachment_11_Analysis_day_of_the_week_by_customer_type.sql

--- Attachment_11_Analysis_day_of_the_week_by_customer_type.sql
+++
@@ -1,4 +0,0 @@
-SELECT day_of_the_week, COUNT(ride_id) AS number_of_rides, member_casual
-FROM `elite-bedrock-410313.1_Bicycle_Case_2023_final.2023_Bicycle_case_study_final` 
-GROUP BY member_casual, day_of_the_week
-ORDER BY day_of_the_week
